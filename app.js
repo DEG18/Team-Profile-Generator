9d7e285b20159ffcce80f9ea27c57991fdd108cf
Luyu Zeng <zengluyu252@gmail.com>
2020-07-28 18:21:18 -0400

    Updated app.js, and output folder

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const axios = require("axios");
-const fs = require("fs");
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
+const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+const allInfoEmployeeArray = new Array();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 //`Employee`, `Manager`, `Engineer`,`Intern`
@@ -28,33 +27,17 @@ const employee = [
   {
     type: "input",
     name: "nameOfEmployee",
-    message: "What is the employee name? ",
+    message: "What is his/her name? ",
   },
   {
     type: "input",
     name: "idOfEmployee",
-    message: "What is the employee ID? ",
+    message: "What is his/her ID? ",
   },
   {
     type: "input",
     name: "emailOfEmployee",
-    message: "What is the employee email? ",
-  },
-];
-
-const engineer = [
-  {
-    type: "input",
-    name: "githubOfEnginerr",
-    message: "What is the GitHub Name? ",
-  },
-];
-
-const intern = [
-  {
-    type: "input",
-    name: "schoolOfInter",
-    message: "What is the School Name? ",
+    message: "What is his/her email address? ",
   },
 ];
 
@@ -62,61 +45,98 @@ const manager = [
   {
     type: "input",
     name: "officeNumberOfManager",
-    message: "What is the Office Number of Manager? ",
+    message: "What is the Manager Office Number? ",
+  },
+];
+
+const engineer = [
+  {
+    type: "input",
+    name: "githubOfEnginerr",
+    message: "What is the Engineer GitHub Name? ",
+  },
+];
+
+const intern = [
+  {
+    type: "input",
+    name: "schoolOfInter",
+    message: "What is the Intern School Name? ",
   },
 ];
 
 // Use .concat method to add 2 questions of array together to a new array
+const questionForManager = employee.concat(manager);
 const questionForEngineer = employee.concat(engineer);
 const questionForInter = employee.concat(intern);
-const questionForManager = employee.concat(manager);
 
-// async function init() {
-//   try {
-//     await inquirer.prompt(positions).then((nameOfPosition) => {
-//       console.log(nameOfPosition);
-//       const positionName = nameOfPosition.positions;
-//       console.log(positionName);
-//       const lowercasePositionName = positionName.toLowerCase();
-//       console.log(lowercasePositionName);
-//       inputInfo(lowercasePositionName);
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
+async function init() {
+  const nameOfPosition = await inquirer.prompt(positions);
+  console.log(nameOfPosition.positions);
 
-function init() {
-  inquirer.prompt(positions).then((nameOfPosition) => {
-    console.log(nameOfPosition);
-    const positionName = nameOfPosition.positions;
-    console.log(positionName);
-    const lowercasePositionName = positionName.toLowerCase();
-    console.log(lowercasePositionName);
-    inputInfo(lowercasePositionName);
+  switch (nameOfPosition.positions) {
+    case "Manager":
+      // code block
+      const infoOfManager = await inquirer.prompt(questionForManager);
+      // constructor(name, id, email, officeNumber) for Manager
+      const addManager = new Manager(
+        infoOfManager.nameOfEmployee,
+        infoOfManager.idOfEmployee,
+        infoOfManager.emailOfEmployee,
+        infoOfManager.officeNumberOfManager
+      );
+      // console.log(addManager);
+      allInfoEmployeeArray.push(addManager);
+      init();
+      break;
+    case "Engineer":
+      // code block
+      const infoOfEngineer = await inquirer.prompt(questionForEngineer);
+      // constructor(name, id, email, github) for Engineer
+      const addEngineer = new Engineer(
+        infoOfEngineer.nameOfEmployee,
+        infoOfEngineer.idOfEmployee,
+        infoOfEngineer.emailOfEmployee,
+        infoOfEngineer.githubOfEnginerr
+      );
+      // console.log(addEngineer);
+      allInfoEmployeeArray.push(addEngineer);
+      init();
+      break;
+    case "Intern":
+      // code block
+      const infoOfIntern = await inquirer.prompt(questionForInter);
+      // constructor(name, id, email, school) for Intern
+      const addIntern = new Intern(
+        infoOfIntern.nameOfEmployee,
+        infoOfIntern.idOfEmployee,
+        infoOfIntern.emailOfEmployee,
+        infoOfIntern.schoolOfInter
+      );
+      // console.log(addIntern);
+      allInfoEmployeeArray.push(addIntern);
+      init();
+      break;
+    case "Exit":
+      // code block
+      console.log("Exit the questions loop!");
+      output();
+      break;
+    default:
+      // code block
+      console.log("Exit the questions loop!");
+  }
+}
+
+function output() {
+  const generatehtml = render(allInfoEmployeeArray);
+  console.log(generatehtml);
+  fs.writeFile(outputPath, generatehtml, function (err) {
+    console.log(err);
   });
 }
 
 init();
-
-// inquirer.prompt(lowercasePositionName).then((infoOfPosition) => {
-//     console.log(infoOfPosition);
-//   });
-
-// async function inputInfo(name) {
-//   try {
-//     const { infoOfPosition } = await inquirer.prompt(name);
-//     console.log(infoOfPosition);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-function inputInfo(name) {
-  inquirer.prompt(name).then((infoOfPosition) => {
-    console.log(infoOfPosition);
-  });
-}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
