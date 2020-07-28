620d7d3ffe5bbe6915ae0063af654458cd55bb8e
Luyu Zeng <zengluyu252@gmail.com>
2020-07-28 00:21:48 -0400

    set questions arrays for Employees and others

--- app.js
+++ app.js
@@ -1,18 +1,122 @@
+const axios = require("axios");
+const fs = require("fs");
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+//`Employee`, `Manager`, `Engineer`,`Intern`
+const positions = [
+  {
+    type: "list",
+    name: "positions",
+    choices: ["Manager", "Engineer", "Intern", "Exit"],
+    message: "Please choose one to add to your Team(choose Exit to exit): ",
+  },
+];
+
+const employee = [
+  {
+    type: "input",
+    name: "nameOfEmployee",
+    message: "What is the employee name? ",
+  },
+  {
+    type: "input",
+    name: "idOfEmployee",
+    message: "What is the employee ID? ",
+  },
+  {
+    type: "input",
+    name: "emailOfEmployee",
+    message: "What is the employee email? ",
+  },
+];
+
+const engineer = [
+  {
+    type: "input",
+    name: "githubOfEnginerr",
+    message: "What is the GitHub Name? ",
+  },
+];
+
+const intern = [
+  {
+    type: "input",
+    name: "schoolOfInter",
+    message: "What is the School Name? ",
+  },
+];
+
+const manager = [
+  {
+    type: "input",
+    name: "officeNumberOfManager",
+    message: "What is the Office Number of Manager? ",
+  },
+];
+
+// Use .concat method to add 2 questions of array together to a new array
+const questionForEngineer = employee.concat(engineer);
+const questionForInter = employee.concat(intern);
+const questionForManager = employee.concat(manager);
+
+// async function init() {
+//   try {
+//     await inquirer.prompt(positions).then((nameOfPosition) => {
+//       console.log(nameOfPosition);
+//       const positionName = nameOfPosition.positions;
+//       console.log(positionName);
+//       const lowercasePositionName = positionName.toLowerCase();
+//       console.log(lowercasePositionName);
+//       inputInfo(lowercasePositionName);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+function init() {
+  inquirer.prompt(positions).then((nameOfPosition) => {
+    console.log(nameOfPosition);
+    const positionName = nameOfPosition.positions;
+    console.log(positionName);
+    const lowercasePositionName = positionName.toLowerCase();
+    console.log(lowercasePositionName);
+    inputInfo(lowercasePositionName);
+  });
+}
+
+init();
+
+// inquirer.prompt(lowercasePositionName).then((infoOfPosition) => {
+//     console.log(infoOfPosition);
+//   });
+
+// async function inputInfo(name) {
+//   try {
+//     const { infoOfPosition } = await inquirer.prompt(name);
+//     console.log(infoOfPosition);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+function inputInfo(name) {
+  inquirer.prompt(name).then((infoOfPosition) => {
+    console.log(infoOfPosition);
+  });
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
