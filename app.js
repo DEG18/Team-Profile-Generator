4f54ff438388e41b4f4bedb4ad09c9a2bbe64169
Luyu Zeng <zengluyu252@gmail.com>
2020-07-28 23:44:06 -0400

    Updated app.js file

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ const allInfoEmployeeArray = new Array();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 //`Employee`, `Manager`, `Engineer`,`Intern`
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
 const positions = [
   {
     type: "list",
@@ -69,6 +74,10 @@ const intern = [
 const questionForManager = employee.concat(manager);
 const questionForEngineer = employee.concat(engineer);
 const questionForInter = employee.concat(intern);
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
 
 async function init() {
   const nameOfPosition = await inquirer.prompt(positions);
@@ -128,32 +137,25 @@ async function init() {
   }
 }
 
-function output() {
-  const generatehtml = render(allInfoEmployeeArray);
-  console.log(generatehtml);
-  fs.writeFile(outputPath, generatehtml, function (err) {
-    console.log(err);
-  });
-}
-
-init();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+function output() {
+  const generatehtml = render(allInfoEmployeeArray);
+  // console.log(generatehtml);
+  fs.writeFile(outputPath, generatehtml, function (err) {
+    console.log(err);
+  });
+}
+
+// Calling init function
+init();
